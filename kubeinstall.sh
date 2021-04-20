4547ed6df0d63ed4294490f9597f36d3d97e0b75
root <root@ip-172-31-40-13.us-east-2.compute.internal>
2021-04-20 06:23:07 +0000

    new

---
+++ kubeinstall.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+sudo apt update
+sudo apt install docker.io -y
+sudo systemctl enable docker
+
+sudo sed -i '/ swap / s/^/#/' /etc/fstab
+
+curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
+
+
+sudo bash -c 'cat << EOF > /etc/apt/sources.list.d/kubernetes.list
+deb http://apt.kubernetes.io/ kubernetes-xenial main
+EOF'
+   
+
+echo "deb http://apt.kubernetes.io/ kubernetes-xenial main" >> ~/kubernetes.list
+sudo mv ~/kubernetes.list /etc/apt/sources.list.d
+sudo apt update
+
+
+sudo apt install -y kubeadm=1.18.0-00 --allow-unauthenticated
+sudo apt install -y kubectl=1.18.0-00 --allow-unauthenticated --allow-downgrades
+sudo apt install -y kubelet=1.18.0-00 --allow-unauthenticated --allow-downgrades
+sudo apt-get install -y kubernetes-cni --allow-unauthenticated
+
+
+sudo kubeadm init
+mkdir -p $HOME/.kube
+sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
+sudo chown $(id -u):$(id -g) $HOME/.kube/config
+
+
+kubectl apply -f "https://cloud.weave.works/k8s/net?k8s-version=$(kubectl version | base64 | tr -d '\n')"
+
+kubectl get pods -A
+

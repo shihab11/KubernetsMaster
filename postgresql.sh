4d36a741db999a605416337b4750f6cc1a503cbb
shihab11 <32284176+shihab11@users.noreply.github.com>
2021-04-26 21:22:56 -0400

    Update postgresql.sh

--- postgresql.sh
+++ postgresql.sh
@@ -9,7 +9,7 @@ sudo apt -y upgrade
 
 wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -
 
-echo "deb http://apt.postgresql.org/pub/repos/apt/ `lsb_release -cs`-pgdg main" |sudo tee  /etc/apt/sources.list.d/pgdg>
+echo "deb http://apt.postgresql.org/pub/repos/apt/ `lsb_release -cs`-pgdg main" |sudo tee  /etc/apt/sources.list.d/pgdg.list
 
 sudo apt update
 sudo apt -y install postgresql-12 postgresql-client-12
